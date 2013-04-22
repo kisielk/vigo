4dbdce176538cf4b7751ca0a49073c5c27c11628
mortdeus <mortdeus@gocos2d.org>
2013-04-21 23:03:04 -0500

    converted visibility to private

--- textobject.go
+++ textobject.go
@@ -4,54 +4,54 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-type TextObjectMode struct {
+type textObjectMode struct {
 	editor *editor
-	object TextObject
+	object textObject
 }
 
-type TextObject struct {
-	Inner bool
-	Kind  TextObjectKind
+type textObject struct {
+	inner bool
+	kind  textObjectKind
 }
 
-type TextObjectKind int
+type textObjectKind int
 
 const (
-	TextObjectWord TextObjectKind = iota
-	TextObjectWhitespaceWord
-	TextObjectSentence
-	TextObjectParagraph
-	TextObjectSection
-	TextObjectPercent
-	TextObjectParens
-	TextObjectBraces
+	textObjectWord textObjectKind = iota
+	textObjectWhitespaceWord
+	textObjectSentence
+	textObjectParagraph
+	textObjectSection
+	textObjectPercent
+	textObjectParens
+	textObjectBraces
 )
 
-func NewTextObjectMode(editor *editor, inner bool) *TextObjectMode {
-	return &TextObjectMode{editor: editor, object: TextObject{Inner: inner}}
+func newTextObjectMode(editor *editor, inner bool) *textObjectMode {
+	return &textObjectMode{editor: editor, object: textObject{inner: inner}}
 }
 
-func (m *TextObjectMode) onKey(ev *termbox.Event) {
+func (m *textObjectMode) onKey(ev *termbox.Event) {
 	switch ev.Ch {
 	case 'w':
-		m.object.Kind = TextObjectWord
+		m.object.kind = textObjectWord
 	case 'W':
-		m.object.Kind = TextObjectWhitespaceWord
-	case 'k':
-		m.object.Kind = TextObjectSentence
+		m.object.kind = textObjectWhitespaceWord
+	case 's':
+		m.object.kind = textObjectSentence
 	case 'p':
-		m.object.Kind = TextObjectParagraph
-	case 'K':
-		m.object.Kind = TextObjectSection
+		m.object.kind = textObjectParagraph
+	case 'S':
+		m.object.kind = textObjectSection
 	case '%':
-		m.object.Kind = TextObjectPercent
+		m.object.kind = textObjectPercent
 	case 'b':
-		m.object.Kind = TextObjectParens
+		m.object.kind = textObjectParens
 	case 'B':
-		m.object.Kind = TextObjectBraces
+		m.object.kind = textObjectBraces
 	}
 }
 
-func (m *TextObjectMode) OnExit() {
+func (m *textObjectMode) OnExit() {
 	//TODO: Return the text object to the previous mode somehow
 }
