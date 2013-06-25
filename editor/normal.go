19afbd9572883fb150609065ebb02d9d96dc3f8d
Kamil Kisiel <kamil@kamilkisiel.net>
2013-06-24 17:14:38 -0700

    Stubbed out ctrl- keybindings in normal mode.

--- editor/normal.go
+++ editor/normal.go
@@ -17,6 +17,8 @@ func newNormalMode(editor *editor) *normalMode {
 }
 
 func (m *normalMode) onKey(ev *termbox.Event) {
+	// Most of the key bindings are derived from those at http://elvis.the-little-red-haired-girl.org/elvisman/elvisvi.html#^Y
+
 	g := m.editor
 	v := g.active.leaf
 
@@ -33,11 +35,69 @@ func (m *normalMode) onKey(ev *termbox.Event) {
 
 	switch ev.Ch {
 	case 0x0:
+		// TODO Cursor centering after Ctrl-U/D seems off.
+		// TODO Ctrl-U and CTRL-D have configurable ranges of motion.
 		switch ev.Key {
-		case termbox.KeySpace:
-			v.onVcommand(viewCommand{Cmd: vCommandMoveCursorForward, Reps: reps})
+		case termbox.KeyCtrlA:
+			//TODO: search for next occurrence of word under cursor
+			return
+		case termbox.KeyCtrlB:
+			//TODO: should move a full page
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfBackward, Reps: reps})
+		case termbox.KeyCtrlD:
+			// TODO: should move by count lines, default to 1/2 screen
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfForward, Reps: reps})
+		case termbox.KeyCtrlE:
+			// TODO: should move by count lines, default to 1
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfForward, Reps: reps})
+		case termbox.KeyCtrlF:
+			//TODO: should move a full page
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfForward, Reps: reps})
+		case termbox.KeyCtrlG:
+			//TODO: Show file status
+			return
+		case termbox.KeyCtrlH:
+			// Same as 'h'
+			// TODO: find a way to avoid duplication of 'h'
+			v.onVcommand(viewCommand{Cmd: vCommandMoveCursorBackward, Reps: reps})
+		case termbox.KeyCtrlJ, termbox.KeyCtrlN:
+			// Same as 'j'
+			// TODO: find a way to avoid duplication of 'j'
+			v.onVcommand(viewCommand{Cmd: vCommandMoveCursorNextLine, Reps: reps})
+		case termbox.KeyCtrlL:
+			// TODO: redraw screen
+			return
+		case termbox.KeyCtrlM:
+			// TODO: move to front of next line
+			return
+		case termbox.KeyCtrlP:
+			// same as 'k'
+			// TODO: find a way to avoid duplication of 'k'
+			v.onVcommand(viewCommand{Cmd: vCommandMoveCursorPrevLine, Reps: reps})
 		case termbox.KeyCtrlR:
 			v.onVcommand(viewCommand{Cmd: vCommandRedo, Reps: reps})
+		case termbox.KeyCtrlU:
+			//TODO: should move by count lines, default to 1/2 screen
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfBackward, Reps: reps})
+		case termbox.KeyCtrlV:
+			//TODO: Start visual selection
+			return
+		case termbox.KeyCtrlW:
+			//TODO: Buffer the ctrl-W and then implement some windowing commands
+			return
+		case termbox.KeyCtrlX:
+			//TODO: Move to column count
+			return
+		case termbox.KeyCtrlY:
+			//TODO: should move by count lines, default to 1
+			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfBackward, Reps: reps})
+		case termbox.KeyEsc:
+			//TODO: Cancel the current command
+			return
+		case termbox.KeySpace:
+			// Same as 'l'
+			// TODO: find a way to avoid duplication of 'l'
+			v.onVcommand(viewCommand{Cmd: vCommandMoveCursorForward, Reps: reps})
 		}
 	case 'h':
 		v.onVcommand(viewCommand{Cmd: vCommandMoveCursorBackward, Reps: reps})
@@ -79,17 +139,6 @@ func (m *normalMode) onKey(ev *termbox.Event) {
 		v.onVcommand(viewCommand{Cmd: vCommandMoveCursorBeginningOfLine})
 	case '$':
 		v.onVcommand(viewCommand{Cmd: vCommandMoveCursorEndOfLine})
-	}
-
-	if ev.Ch == 0x0 {
-		switch ev.Key {
-		// TODO Cursor centering after Ctrl-U/D seems off.
-		// TODO Ctrl-U and CTRL-D have configurable ranges of motion.
-		case termbox.KeyCtrlU, termbox.KeyCtrlB:
-			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfBackward, Reps: reps})
-		case termbox.KeyCtrlD, termbox.KeyCtrlF:
-			v.onVcommand(viewCommand{Cmd: vCommandMoveViewHalfForward, Reps: reps})
-		}
 	}
 
 	// TODO use reps to set range for command mode
